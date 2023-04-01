d4dfe7b84f816f54440566b5146dd82aa4f32ead
Jan Schellner <Jan.Schellner@escriba.de>
2023-04-01 14:49:46 +0200

    initial commit

---
+++ src/main/java/de/schellner/azuretest/AzureTestApplication.java
@@ -0,0 +1,21 @@
+package de.schellner.azuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzureTestApplication {
+
+	@GetMapping("/helloWorld")
+	public String helloWorld() {
+		return "Hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureTestApplication.class, args);
+	}
+
+}
